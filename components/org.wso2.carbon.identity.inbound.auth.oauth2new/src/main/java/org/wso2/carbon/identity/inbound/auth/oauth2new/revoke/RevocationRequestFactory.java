038a5b692135da1499876f947e90e4e4aecdf250
Johann Dilantha Nallathamby <johann@Johanns-MacBook-Pro.local>
2016-11-13 12:17:31 +0530

    Fixing implementation to work with latest changes in master

---
+++ components/org.wso2.carbon.identity.inbound.auth.oauth2new/src/main/java/org/wso2/carbon/identity/inbound/auth/oauth2new/revoke/RevocationRequestFactory.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.inbound.auth.oauth2new.revoke;
+
+import org.apache.commons.lang.StringUtils;
+import org.wso2.carbon.identity.application.authentication.framework.inbound.FrameworkClientException;
+import org.wso2.carbon.identity.application.authentication.framework.inbound.IdentityRequest;
+import org.wso2.carbon.identity.inbound.auth.oauth2new.bean.message.request.OAuth2IdentityRequestFactory;
+import org.wso2.carbon.identity.inbound.auth.oauth2new.exception.OAuth2ClientException;
+import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RevocationRequestFactory extends OAuth2IdentityRequestFactory {
+
+    @Override
+    public String getName() {
+        return "RevocationRequestFactory";
+    }
+
+    @Override
+    public boolean canHandle(HttpServletRequest request, HttpServletResponse response) {
+        if(StringUtils.isNotBlank(request.getParameter("token"))) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public RevocationRequest.RevokeRequestBuilder create(HttpServletRequest request,
+                                                         HttpServletResponse response) throws OAuth2ClientException {
+
+        RevocationRequest.RevokeRequestBuilder builder = new RevocationRequest.RevokeRequestBuilder(request, response);
+        try {
+            super.create(builder, request, response);
+        } catch (FrameworkClientException e) {
+            throw OAuth2ClientException.error(e.getMessage(), e);
+        }
+        builder.setToken(request.getParameter("token"));
+        builder.setTokenTypeHint(request.getParameter("token_type_hint"));
+        builder.setCallback(request.getParameter("callback"));
+        return builder;
+    }
+
+    @Override
+    public void create(IdentityRequest.IdentityRequestBuilder builder, HttpServletRequest request,
+                       HttpServletResponse response)
+            throws OAuth2ClientException {
+
+        RevocationRequest.RevokeRequestBuilder revokeRequestBuilder = (RevocationRequest.RevokeRequestBuilder)builder;
+        try {
+            super.create(revokeRequestBuilder, request, response);
+        } catch (FrameworkClientException e) {
+            throw OAuth2ClientException.error(e.getMessage(), e);
+        }
+        revokeRequestBuilder.setToken(request.getParameter("token"));
+        revokeRequestBuilder.setTokenTypeHint(request.getParameter("token_type_hint"));
+        revokeRequestBuilder.setCallback(request.getParameter("callback"));
+    }
+}
---
+++ components/org.wso2.carbon.identity.inbound.auth.oidc/src/main/java/org/wso2/carbon/identity/inbound/auth/oidc/bean/message/request/authz/OIDCAuthzRequestFactory.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.inbound.auth.oidc.bean.message.request.authz;
+
+import org.apache.oltu.oauth2.common.OAuth;
+import org.wso2.carbon.identity.application.authentication.framework.inbound.IdentityRequest;
+import org.wso2.carbon.identity.inbound.auth.oauth2new.bean.message.request.authz.AuthzRequestFactory;
+import org.wso2.carbon.identity.inbound.auth.oauth2new.exception.OAuth2ClientException;
+import org.wso2.carbon.identity.inbound.auth.oauth2new.util.OAuth2Util;
+import org.wso2.carbon.identity.inbound.auth.oidc.OIDC;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Set;
+
+public class OIDCAuthzRequestFactory extends AuthzRequestFactory {
+
+    @Override
+    public String getName() {
+        return "OIDCAuthzRequestFactory";
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public boolean canHandle(HttpServletRequest request, HttpServletResponse response) {
+        if(super.canHandle(request, response)) {
+            Set<String> scopes = OAuth2Util.buildScopeSet(request.getParameter(OAuth.OAUTH_SCOPE));
+            if (scopes.contains(OIDC.OPENID_SCOPE)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public OIDCAuthzRequest.OIDCAuthzRequestBuilder create(HttpServletRequest request,
+                                                           HttpServletResponse response) throws OAuth2ClientException {
+
+        OIDCAuthzRequest.OIDCAuthzRequestBuilder builder = new OIDCAuthzRequest.OIDCAuthzRequestBuilder
+                (request, response);
+        super.create(builder, request, response);
+        builder.setNonce(request.getParameter(OIDC.NONCE));
+        builder.setDisplay(request.getParameter(OIDC.DISPLAY));
+        builder.setIdTokenHint(request.getParameter(OIDC.ID_TOKEN_HINT));
+        builder.setLoginHint(request.getParameter(OIDC.LOGIN_HINT));
+        Set<String> prompts = OAuth2Util.buildScopeSet(request.getParameter(OIDC.PROMPT));
+        if(prompts.contains(OIDC.Prompt.NONE) && prompts.size() > 1){
+            throw OAuth2ClientException.error("Prompt value 'none' cannot be used with other " +
+            "prompts. Prompt: " + request.getParameter(OIDC.PROMPT));
+        }
+        builder.setPrompts(prompts);
+        if (prompts.contains(OIDC.Prompt.LOGIN)) {
+            builder.setLoginRequired(true);
+        }
+        if(prompts.contains(OIDC.Prompt.CONSENT)) {
+            builder.setConsentRequired(true);
+        }
+        return builder;
+    }
+
+    @Override
+    public void create(IdentityRequest.IdentityRequestBuilder builder, HttpServletRequest request,
+                       HttpServletResponse response)
+            throws OAuth2ClientException {
+
+        OIDCAuthzRequest.OIDCAuthzRequestBuilder oidcAuthzRequestBuilder =
+                (OIDCAuthzRequest.OIDCAuthzRequestBuilder)builder;
+        super.create(oidcAuthzRequestBuilder, request, response);
+        oidcAuthzRequestBuilder.setNonce(request.getParameter(OIDC.NONCE));
+        oidcAuthzRequestBuilder.setDisplay(request.getParameter(OIDC.DISPLAY));
+        oidcAuthzRequestBuilder.setIdTokenHint(request.getParameter(OIDC.ID_TOKEN_HINT));
+        oidcAuthzRequestBuilder.setLoginHint(request.getParameter(OIDC.LOGIN_HINT));
+        Set<String> prompts = OAuth2Util.buildScopeSet(request.getParameter(OIDC.PROMPT));
+        if(prompts.contains(OIDC.Prompt.NONE) && prompts.size() > 1){
+            throw OAuth2ClientException.error("Prompt value 'none' cannot be used with other " +
+                                              "prompts. Prompt: " + request.getParameter(OIDC.PROMPT));
+        }
+        oidcAuthzRequestBuilder.setPrompts(prompts);
+        if (prompts.contains(OIDC.Prompt.LOGIN)) {
+            oidcAuthzRequestBuilder.setLoginRequired(true);
+        }
+        if(prompts.contains(OIDC.Prompt.CONSENT)) {
+            oidcAuthzRequestBuilder.setConsentRequired(true);
+        }
+    }
+}
